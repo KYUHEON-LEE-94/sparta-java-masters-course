397085297e7cd33794f459ea81028327863c468d
Maizurzu <panpan68@naver.com>
2024-12-23 22:28:29 +0900

    Initial commit

---
+++ src/main/java/com/lecture/springmasters/SpringMastersApplication.java
@@ -0,0 +1,13 @@
+package com.lecture.springmasters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMastersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMastersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lecture/springmasters/SpringMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.springmasters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMastersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
