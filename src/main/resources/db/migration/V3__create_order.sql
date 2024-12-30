7a214aa2c91410336c7208a1d8a9a05f43bd4810
Maizurzu <panpan68@naver.com>
2024-12-30 21:23:18 +0900

    sql V1__init에 통합

--- src/main/resources/db/migration/V3__create_order.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE orders (
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        user_id BIGINT NOT NULL COMMENT '연관된 사용자 ID',
-                        total_price DECIMAL(10, 2) NOT NULL,
-                        status VARCHAR(20) DEFAULT 'pending' COMMENT 'pending, completed, canceled',
-                        shipping_address TEXT NOT NULL,
-                        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
