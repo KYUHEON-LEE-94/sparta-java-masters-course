2f7dc1bb8c04bd0612451cec7e10198601c6a0af
Maizurzu <panpan68@naver.com>
2025-01-10 22:53:51 +0900

    OrderProcessServiceTest 실습

--- src/test/java/com/lecture/springmasters/domain/order/service/OrderProcessServiceTest.java
+++ src/test/java/com/lecture/springmasters/domain/order/service/OrderProcessServiceTest.java
@@ -8,6 +8,8 @@ import com.lecture.springmasters.common.exception.ServiceExceptionCode;
 import com.lecture.springmasters.domain.order.dto.OrderProductRequest;
 import com.lecture.springmasters.domain.order.dto.OrderRequest;
 import com.lecture.springmasters.entity.Order;
+import com.lecture.springmasters.entity.OrderItem;
+import com.lecture.springmasters.entity.Product;
 import com.lecture.springmasters.entity.User;
 import com.lecture.springmasters.repository.OrderRepository;
 import com.lecture.springmasters.repository.ProductRepository;
@@ -68,4 +70,52 @@ class OrderProcessServiceTest {
     assertEquals(ServiceExceptionCode.NOT_FOUND_PRODUCT.getCode(), exception.getCode());
 
   }
+
+  @Test
+  void shouldCalculateTotalPriceCorrectly() {
+    // Given
+    OrderItem item1 = new OrderItem(null, null, 2, BigDecimal.valueOf(100));
+    OrderItem item2 = new OrderItem(null, null, 3, BigDecimal.valueOf(200));
+
+    List<OrderItem> orderItems = Arrays.asList(item1, item2);
+    BigDecimal expectedTotalPrice = BigDecimal.valueOf(800); // (2 * 100) + (3 * 200)
+
+    // When
+    BigDecimal totalPrice = orderProcessService.calculateTotalPrice(orderItems);
+
+    // Then
+    assertEquals(expectedTotalPrice, totalPrice);
+  }
+
+  @Test
+  @Transactional
+  void 요청_주문수량이_재고수량과_같은_경우() {
+    //given
+    User user = userRepository.findById(1L).get();
+    Order order = orderRepository.save(
+        Order.builder().user(user).totalPrice(BigDecimal.ZERO).build());
+
+    Product product = productRepository.findById(1L).get();
+    Integer stock = product.getStock();
+
+    OrderProductRequest productRequest = new OrderProductRequest();
+    ReflectionTestUtils.setField(productRequest, "id", 1L);
+    ReflectionTestUtils.setField(productRequest, "quantity", stock);
+
+    List<OrderProductRequest> orderProductRequests = Arrays.asList(productRequest);
+
+    OrderRequest request = new OrderRequest();
+    ReflectionTestUtils.setField(request, "userId", 1L);
+    ReflectionTestUtils.setField(request, "products", orderProductRequests);
+
+    // When
+    List<OrderItem> orderItems = orderProcessService.createOrderItems(request, order);
+
+    // Then
+    assertEquals(product.getId(), orderItems.get(0).getProduct().getId());
+    assertEquals(stock, orderItems.get(0).getQuantity());
+
+  }
+
+
 }
